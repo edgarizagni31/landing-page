b26017f80e17581acfee8525b2cfcf0aaa065b02
edgarizagni31 <ochoaEdgar3108@gmail.com>
2021-04-02 22:22:10 -0500

    update file tree

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,22 @@
-const path = require("path");
+const path = require('path');
 
 //plugins
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlMinimizerWebpackPlugin = require('html-minimizer-webpack-plugin')
-const MiniCssWebpackPlugin = require("mini-css-extract-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssWebpackPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    entry: "./index.js",
+    entry: path.resolve(__dirname, 'src', 'index.js' ),
     
     output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "bundle.js",
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
     },
 
     resolve: {
-        extensions: [".js"],
+        extensions: ['.js'],
     },
 
     module: {
@@ -25,7 +25,12 @@ module.exports = {
                 test: /\.(sc|c)ss$/,
                 use: [
                     MiniCssWebpackPlugin.loader,
-                    'css-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            url: false
+                        }
+                    },
                     {
                         loader: 'sass-loader',
                         options: {
@@ -36,50 +41,56 @@ module.exports = {
                         loader: 'sass-resources-loader',
                         options:  {
                             resources: [
-                                './sass/base/__nomalize.scss',
-                                './sass/base/__base.scss',
-                                './sass/components/__header.scss',
-                                './sass/components/__about.scss',
-                                './sass/components/__creations.scss',
-                                './sass/components/__footer.scss'
+                                './src/sass/base/__nomalize.scss',
+                                './src/sass/base/__base.scss',
+                                './src/sass/components/__header.scss',
+                                './src/sass/components/__about.scss',
+                                './src/sass/components/__creations.scss',
+                                './src/sass/components/__footer.scss'
                             ],
-                            hoistUseStatements: true
                         }
                     },
                 ],
             },
             {
                 test: /\.(jpg|png)/,
-                type: "asset/resource"
+                type: 'asset/resource'
             }
         ]
     },
 
     plugins: [
+        new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             inject: true,
-            template: "./index.html",
-            filename: "index.html",
+            template: './index.html',
+            filename: 'index.html',
         }),
-        new CleanWebpackPlugin(),
         new MiniCssWebpackPlugin({
-            filename: "./style.css"
+            filename: './style.css'
         }),
         new CopyWebpackPlugin(
             {
                 patterns: [
                     {
-                        from: path.resolve(__dirname,"images"),
-                        to: './images'
+                        from: path.resolve(__dirname,"assets"),
+                        to: './assets'
                     }
                 ]
             }
         )
     ],
+
     optimization:{
         minimize: true,
         minimizer: [
             new  HtmlMinimizerWebpackPlugin()
         ]
+    },
+
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        compress: true,
+        port: 3000
     }
 };
