29217b6cca0a6c49e16d782c228ca86ddc58cb5d
edgarizagni31 <ochoaEdgar3108@gmail.com>
2021-04-14 21:44:42 -0500

    include babel

---
+++ docs/js/index.js
@@ -0,0 +1,19 @@
+"use strict";
+
+displayMenu.addEventListener('touchstart', handleMenu);
+
+function handleMenu() {
+  var displayMenu = document.getElementById("displayMenu");
+  var menu = document.querySelector(".navbar__menu");
+
+  if (!displayMenu.classList.contains('icon-close')) {
+    displayMenu.classList.add("icon-close");
+    displayMenu.setAttribute("src", "./assets/icons/icon-close.svg");
+  } else {
+    displayMenu.classList.remove("icon-close");
+    displayMenu.setAttribute("src", "./assets/icons/icon-hamburger.svg");
+  }
+
+  ;
+  menu.classList.toggle("navbar__menu--display");
+}
