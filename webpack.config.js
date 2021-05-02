c79759c5cd6fd9ffc6aa095a29def019bdc231cf
edgarizagni31 <ochoaEdgar3108@gmail.com>
2021-05-02 12:18:20 -0500

    webpack v1.0

---
+++ webpack.config.js
@@ -0,0 +1,82 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniExtractCssPlugin = require('mini-css-extract-plugin');
+const copyWebpackPlugin = require('copy-webpack-plugin');
+const CssMinimizer = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname,'dist'),
+        filename: 'index.js',
+        assetModuleFilename: "assets/images/[hash][ext]"
+    },
+    resolve: {
+        extensions: ['.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [ MiniExtractCssPlugin.loader, 'css-loader']
+            },
+            {
+                test: /\.jpg$/,
+                type: 'asset/resource'
+            },
+            {
+                test: /\.ttf$/,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 1000,
+                        mimetype: 'application/font-ttf',
+                        name: '[name].[contenthash].[ext]',
+                        outputPath: './assets/fonts/',
+                        publicPath: './assets/fonts/',
+                        esModule: true,
+                    }
+                } 
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: './public/index.html',
+            filename: 'index.html'
+        }),
+        new MiniExtractCssPlugin({
+            filename: './styles.css'
+        }),
+        new copyWebpackPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, 'src', 'assets/images'),
+                    to: 'assets/images'
+                },
+                {
+                    from: path.resolve(__dirname,'src','assets/icons'),
+                    to: 'assets/icons'
+                }
+            ]
+        })
+    ],
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new CssMinimizer(),
+            new TerserPlugin()
+        ]
+    }
+}
