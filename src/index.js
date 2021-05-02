99647750ac9569adc05cbb3332210519b39be411
edgarizagni31 <ochoaEdgar3108@gmail.com>
2021-05-02 12:17:57 -0500

    update tree files

---
+++ src/index.js
@@ -0,0 +1,20 @@
+require('./styles.css');
+
+const displayMenu = document.getElementById('displayMenu');
+const classListDisplayMenu = displayMenu.classList;
+const pathAssets = './assets/icons/'; 
+
+displayMenu.addEventListener('click', handleMenu);
+
+function handleMenu() {
+  if ( !classListDisplayMenu.contains('icon-close') ) {
+    classListDisplayMenu.add('icon-close');
+    displayMenu.setAttribute('src', `${pathAssets}/icon-close.svg`);
+  }else {
+    classListDisplayMenu.remove('icon-close');
+    displayMenu.setAttribute("src", `${pathAssets}/icon-hamburger.svg`);
+  }
+
+  document.querySelector('.header__menu').classList.toggle("header__menu--display");
+  document.querySelector('.header__title').classList.toggle("header__title--menu");
+}
