4d37be219c1de862f849b9636a1b66ed60de55ea
edgarizagni31 <ochoaEdgar3108@gmail.com>
2021-05-03 10:02:47 -0500

    update styles

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import './styles.css';
 
 const displayMenu = document.getElementById('displayMenu');
-const classListDisplayMenu = displayMenu.classList;
-const pathAssets = './assets/icons/'; 
 
-displayMenu.addEventListener('click', handleMenu);
+document.addEventListener('DOMContentLoaded', () => init() );
+
+function init() {
+  const menuItems = document.querySelectorAll('.header__menu-items');
+
+  displayMenu.addEventListener('click', handleMenu); 
+  menuItems.forEach(menuItem => {
+    menuItem.addEventListener('click', handleMenuItem);
+  });
+}
 
 function handleMenu() {
+  const classListDisplayMenu = displayMenu.classList;
+  const pathAssets = './assets/icons/'; 
+
   if ( !classListDisplayMenu.contains('icon-close') ) {
     classListDisplayMenu.add('icon-close');
     displayMenu.setAttribute('src', `${pathAssets}/icon-close.svg`);
@@ -14,7 +24,10 @@ function handleMenu() {
     classListDisplayMenu.remove('icon-close');
     displayMenu.setAttribute("src", `${pathAssets}/icon-hamburger.svg`);
   }
-
   document.querySelector('.header__menu').classList.toggle("header__menu--display");
   document.querySelector('.header__title').classList.toggle("header__title--menu");
+}
+
+function handleMenuItem() {
+  handleMenu();
 }
